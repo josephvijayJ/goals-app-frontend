fcdba54892e2d21768c743c18a3212e08984b9f5
Joseph Vijay J <87023714+josephvijayJ@users.noreply.github.com>
2022-04-09 14:25:22 +0530

    Update goalService.js

--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -10,7 +10,7 @@ const createGoal = async (goalData, token) => {
     },
   }
 
-  const response = await axios.post(API_URL, goalData, config)
+  const response = await axios.post('https://goals-app-backend123.herokuapp.com/api/goals', goalData, config)
 
   return response.data
 }
@@ -23,7 +23,7 @@ const getGoals = async (token) => {
     },
   }
 
-  const response = await axios.get(API_URL, config)
+  const response = await axios.get('https://goals-app-backend123.herokuapp.com/api/goals', config)
 
   return response.data
 }
@@ -36,7 +36,7 @@ const deleteGoal = async (goalId, token) => {
     },
   }
 
-  const response = await axios.delete(API_URL + goalId, config)
+  const response = await axios.delete('https://goals-app-backend123.herokuapp.com/api/goals/goalId', config)
 
   return response.data
 }
