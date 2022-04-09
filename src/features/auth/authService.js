71db8d34a19bcf92872184e5810916114c50bb85
Joseph Vijay J <87023714+josephvijayJ@users.noreply.github.com>
2022-04-09 14:23:05 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -4,7 +4,7 @@ const API_URL = 'https://goals-app-backend123.herokuapp.com/api/users/'
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post('https://goals-app-backend123.herokuapp.com/api/users', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
@@ -15,7 +15,7 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post('https://goals-app-backend123.herokuapp.com/api/users/login', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
